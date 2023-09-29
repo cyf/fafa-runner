85406d7d5ebb07ba7d7bde7f090f3e7295b4f226
kjxbyz <kjxbyz@163.com>
2023-09-29 20:17:16 +0800

    update `Privacy` & `Terms and Conditions`

--- next.config.js
+++ next.config.js
@@ -1,22 +1,18 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
-});
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/fafa-runner",
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    domains: [
+      "www.kjxbyz.com",
+    ],
+  },
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = withContentlayer(nextConfig);
