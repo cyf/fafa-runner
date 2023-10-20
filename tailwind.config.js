432abf3f1ebf04edeb7687a28e3316db86268d73
kjxbyz <kjxbyz@163.com>
2023-10-20 21:03:28 +0800

    fix: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   future: {
@@ -60,6 +60,7 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
