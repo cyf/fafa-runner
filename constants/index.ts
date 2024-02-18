4209a5332be8890550fe4a96c67e3d0a7ae18ac7
kjxbyz <kjxbyz@163.com>
2024-02-18 20:03:40 +0800

    docs: update README.md

--- constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const cacheLngKey: string = "__fafa_runner_lng__";
-export const cacheThemeKey: string = "__fafa_runner_theme__";
-export const basePath =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/fafa-runner" : "";
